30a543059506ab4188078402a861dad3059f90f0
Ryan Parman <ryan@ryanparman.com>
2016-07-17 13:28:13 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,72 @@
+# PHPUnit Result Printer
+
+[![Source](http://img.shields.io/badge/source-skyzyx/phpunit-result-printer-blue.svg?style=flat-square)](https://github.com/skyzyx/phpunit-result-printer)
+[![Latest Stable Version](http://img.shields.io/packagist/v/skyzyx/phpunit-result-printer.svg?style=flat-square)](https://packagist.org/packages/skyzyx/phpunit-result-printer)
+[![Total Downloads](http://img.shields.io/packagist/dt/skyzyx/phpunit-result-printer.svg?style=flat-square)](https://packagist.org/packages/skyzyx/phpunit-result-printer)
+[![Open Issues](http://img.shields.io/github/issues/skyzyx/phpunit-result-printer.svg?style=flat-square)](https://github.com/skyzyx/phpunit-result-printer/issues)
+[![License](http://img.shields.io/packagist/l/skyzyx/phpunit-result-printer-blue.svg?style=flat-square)](https://packagist.org/packages/skyzyx/phpunit-result-printer)
+[![Author](http://img.shields.io/badge/author-@skyzyx-blue.svg?style=flat-square)](https://twitter.com/skyzyx)
+
+A custom result printer for PHPUnit.
+
+
+## Examples
+
+{Fill-in: Example usage of this code.}
+
+
+## Installation
+
+Using [Composer]:
+```bash
+composer require skyzyx/phpunit-result-printer=^1.0
+```
+
+And include it in your scripts:
+
+```php
+require_once 'vendor/autoload.php';
+```
+
+
+## Testing
+
+Firstly, run `composer install --optimize-autoloader` to download and install the dependencies.
+
+You can run the tests as follows:
+```bash
+bin/phpunit
+```
+
+
+## Contributing
+Here's the process for contributing:
+
+1. Fork PHPUnit Result Printer to your GitHub account.
+2. Clone your GitHub copy of the repository into your local workspace.
+3. Write code, fix bugs, and add tests with 100% code coverage.
+4. Commit your changes to your local workspace and push them up to your GitHub copy.
+5. You submit a GitHub pull request with a description of what the change is.
+6. The contribution is reviewed. Maybe there will be some banter back-and-forth in the comments.
+7. If all goes well, your pull request will be accepted and your changes are merged in.
+
+
+## Authors, Copyright & Licensing
+
+* Copyright (c) 2014-2016 [Ryan Parman](http://ryanparman.com).
+
+See also the list of [contributors](https://github.com/skyzyx/phpunit-result-printer/contributors) who participated in this project.
+
+Licensed for use under the terms of the [MIT] license.
+
+
+## Coding Standards
+
+PSR-0/1/2 are a solid foundation, but are not an entire coding style by themselves. I have taken the time to document all of the nitpicky patterns and nuances of my personal coding style. It goes well-beyond brace placement and tabs vs. spaces to cover topics such as docblock annotations, ternary operations and which variation of English to use. It aims for thoroughness and pedanticism over hoping that we can all get along.
+
+<https://github.com/skyzyx/php-coding-standards>
+
+  [PHP]: http://php.net
+  [Composer]: https://getcomposer.org
+  [MIT]: http://www.opensource.org/licenses/mit-license.php
+  [Apache 2.0]: http://opensource.org/licenses/Apache-2.0
---
+++ ResultPrinter.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Copyright (c) 2014-2016 Ryan Parman
+ */
+
+namespace Skyzyx\ResultPrinter;
+
+class ResultPrinter extends PHPUnit_TextUI_ResultPrinter
+{
+    /** @var string */
+    protected $test_name_status = '';
+
+    /** @var string */
+    protected $errorEmoji = "!"; // Single exclamation
+
+    /** @var string */
+    protected $failureEmoji = "\xE2\x80\xBC"; // Double exclamation
+
+    /** @var string */
+    protected $incompleteEmoji = "I";
+
+    /** @var string */
+    protected $riskyEmoji = "R";
+
+    /** @var string */
+    protected $skippedEmoji = "S";
+
+    /** @var string */
+    protected $passEmoji = "\xE2\x9c\x93"; // Checkmark
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function writeProgress($progress)
+    {
+        switch ($progress) {
+            case 'E':
+                return parent::writeProgress(sprintf('%s %s',
+                    $this->errorEmoji,
+                    $this->test_name_status));
+
+            case 'F':
+                return parent::writeProgress(sprintf('%s %s',
+                    $this->failureEmoji,
+                    $this->test_name_status));
+
+            case 'I':
+                return parent::writeProgress(sprintf('%s %s',
+                    $this->incompleteEmoji,
+                    $this->test_name_status));
+
+            case 'R':
+                return parent::writeProgress(sprintf('%s %s',
+                    $this->riskyEmoji,
+                    $this->test_name_status));
+
+            case 'S':
+                return parent::writeProgress(sprintf('%s %s',
+                    $this->skippedEmoji,
+                    $this->test_name_status));
+
+            case '.':
+                return parent::writeProgress(sprintf('%s %s',
+                    $this->passEmoji,
+                    $this->test_name_status));
+
+            default:
+                return parent::writeProgress(sprintf('%s %s',
+                    $progress,
+                    $this->test_name_status));
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function startTestSuite(\PHPUnit_Framework_TestSuite $suite)
+    {
+        if ($this->numTests == -1) {
+            $this->numTests      = count($suite);
+            $this->numTestsWidth = strlen((string) $this->numTests);
+            $this->maxColumn     = 0;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function endTest(\PHPUnit_Framework_Test $test, $time)
+    {
+        $test_name = \PHPUnit_Util_Test::describe($test);
+
+        if (!empty($test_name)) {
+            $this->test_name_status = sprintf("%s (%s)\n",
+                $test_name,
+                sprintf("%s ms",
+                    round($time * 1000)));
+        }
+
+        if (!$this->lastTestFailed) {
+            $this->writeProgress('.');
+        }
+
+        if ($test instanceof \PHPUnit_Framework_TestCase) {
+            $this->numAssertions += $test->getNumAssertions();
+        } elseif ($test instanceof \PHPUnit_Extensions_PhptTestCase) {
+            $this->numAssertions++;
+        }
+
+        $this->lastTestFailed = false;
+
+        if ($test instanceof \PHPUnit_Framework_TestCase) {
+            if (method_exists($this, 'hasExpectationOnOutput') && !$test->hasExpectationOnOutput()) {
+                $this->write($test->getActualOutput());
+            }
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function writeProgressWithColor($color, $buffer)
+    {
+        return $this->writeProgress($buffer);
+    }
+}
